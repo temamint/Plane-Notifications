3dbcd196630b46ccd95bea331b50ba54d8714c38
Tema Mint <blago22022@gmail.com>
2025-06-03 10:04:47 +0300

    update req

--- routes/projects.js
+++ routes/projects.js
@@ -16,7 +16,7 @@ router.get('/', async (req, res) => {
 		const projectsWithMembers = await Promise.all(
 			projects.map(async (project) => {
 				try {
-					const memberRes = await planeApi.get(`/workspaces/:${process.env.PLANE_WORKSPACE_SLUG}/projects/${project.id}/members/`);
+					const memberRes = await planeApi.get(`/workspaces/${process.env.PLANE_WORKSPACE_SLUG}/projects/${project.id}/members/`);
 					const members = memberRes.data.results || [];
 					return { ...project, members };
 				} catch (err) {
